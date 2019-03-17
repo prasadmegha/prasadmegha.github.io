dbaad14cf8716a54f2d9c265bdbaa3fd7e1cd45e
Megha Prasad <meghaprasad01@gmail.com>
2019-03-17 09:21:00 -0700

    Add files via upload
    iniitial changes

---
+++ script1.js
@@ -0,0 +1,642 @@
+var margin = {left:80, top:40, right:120, bottom:50},
+	width = Math.max( Math.min(window.innerWidth, 1200) - margin.left - margin.right - 20, 400),
+    height = Math.max( Math.min(window.innerHeight - 250, 1300) - margin.top - margin.bottom - 20, 400),
+    innerRadius = Math.min(width * 0.33, height * .45),
+    outerRadius = innerRadius * 1.05;
+	
+//Recalculate the width and height now that we know the radius
+width = outerRadius * 2 + margin.right + margin.left;
+height = outerRadius * 2 + margin.top + margin.bottom;
+	
+//Reset the overall font size
+var newFontSize = Math.min(70, Math.max(40, innerRadius * 62.5 / 250));
+d3.select("html").style("font-size", newFontSize + "%");
+
+////////////////////////////////////////////////////////////
+////////////////// Set-up Chord parameters /////////////////
+////////////////////////////////////////////////////////////
+	
+var pullOutSize = 20 + 30/135 * innerRadius;
+var numFormat = d3.format(",.0f");
+var defaultOpacity = 0.85,
+	fadeOpacity = 0.075;
+						
+var loom = d3.loom()
+    .padAngle(0.05)
+	//.sortSubgroups(sortAlpha)
+	//.heightInner(28)
+	.emptyPerc(0.2)
+	.widthInner(30)
+	//.widthInner(function(d) { return 6 * d.length; })
+	.value(function(d) { return d.count; })
+	.inner(function(d) { return d.Degree; })
+	.outer(function(d) { return d.Sector; });
+
+var arc = d3.arc()
+    .innerRadius(innerRadius*1.01)
+    .outerRadius(outerRadius);
+
+var string = d3.string()
+    .radius(innerRadius)
+	.pullout(pullOutSize);
+
+////////////////////////////////////////////////////////////
+//////////////////// Character notes ///////////////////////
+////////////////////////////////////////////////////////////
+	
+var characterNotes = [];
+characterNotes["BS"] = "BS degree holders are ...";
+characterNotes["BA"] = "BA degree holders are ...";
+characterNotes["BE"] = "BE degree holders are ...";
+characterNotes["MD"] = "MD degree holders are ...";
+characterNotes["MS"] = "MS degree holders are ..."; 
+characterNotes["MBA"] = "MBA degree holders are ..."; 
+characterNotes["PhD"] = "PHD degree holders are ...";
+
+////////////////////////////////////////////////////////////
+////////////////////// Create SVG //////////////////////////
+////////////////////////////////////////////////////////////
+			
+var svg = d3.select("#lotr-chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+
+dataAgg = [
+  {
+    "Sector": "biotechology",
+    "Degree": "BS",
+    "count": 1
+  },
+  {
+    "Sector": "biotechology",
+    "Degree": "MBA",
+    "count": 3
+  },
+  {
+    "Sector": "biotechology",
+    "Degree": "MS",
+    "count": 2
+  },
+  {
+    "Sector": "biotechology",
+    "Degree": "PhD",
+    "count": 16
+  },
+  {
+    "Sector": "enterprise software",
+    "Degree": "BA",
+    "count": 1
+  },
+  {
+    "Sector": "enterprise software",
+    "Degree": "BS",
+    "count": 7
+  },
+  {
+    "Sector": "enterprise software",
+    "Degree": "MBA",
+    "count": 8
+  },
+  {
+    "Sector": "enterprise software",
+    "Degree": "MS",
+    "count": 3
+  },
+  {
+    "Sector": "enterprise software",
+    "Degree": "PhD",
+    "count": 5
+  },
+  {
+    "Sector": "games",
+    "Degree": "BA",
+    "count": 4
+  },
+  {
+    "Sector": "games",
+    "Degree": "BE",
+    "count": 1
+  },
+  {
+    "Sector": "games",
+    "Degree": "BS",
+    "count": 3
+  },
+  {
+    "Sector": "games",
+    "Degree": "MA",
+    "count": 1
+  },
+  {
+    "Sector": "games",
+    "Degree": "MBA",
+    "count": 7
+  },
+  {
+    "Sector": "games",
+    "Degree": "MS",
+    "count": 4
+  },
+  {
+    "Sector": "games",
+    "Degree": "PhD",
+    "count": 2
+  },
+  {
+    "Sector": "health care",
+    "Degree": "BA",
+    "count": 2
+  },
+  {
+    "Sector": "health care",
+    "Degree": "BS",
+    "count": 8
+  },
+  {
+    "Sector": "health care",
+    "Degree": "MBA",
+    "count": 7
+  },
+  {
+    "Sector": "health care",
+    "Degree": "MD",
+    "count": 3
+  },
+  {
+    "Sector": "health care",
+    "Degree": "MS",
+    "count": 3
+  },
+  {
+    "Sector": "health care",
+    "Degree": "PhD",
+    "count": 7
+  },
+  {
+    "Sector": "manufacturing",
+    "Degree": "BA",
+    "count": 3
+  },
+  {
+    "Sector": "manufacturing",
+    "Degree": "BE",
+    "count": 3
+  },
+  {
+    "Sector": "manufacturing",
+    "Degree": "BS",
+    "count": 3
+  },
+  {
+    "Sector": "manufacturing",
+    "Degree": "MBA",
+    "count": 6
+  },
+  {
+    "Sector": "manufacturing",
+    "Degree": "MS",
+    "count": 3
+  },
+  {
+    "Sector": "manufacturing",
+    "Degree": "PhD",
+    "count": 3
+  },
+  {
+    "Sector": "medical",
+    "Degree": "BA",
+    "count": 1
+  },
+  {
+    "Sector": "medical",
+    "Degree": "BS",
+    "count": 3
+  },
+  {
+    "Sector": "medical",
+    "Degree": "MBA",
+    "count": 3
+  },
+  {
+    "Sector": "medical",
+    "Degree": "MS",
+    "count": 1
+  },
+  {
+    "Sector": "medical devices",
+    "Degree": "BS",
+    "count": 2
+  },
+  {
+    "Sector": "medical devices",
+    "Degree": "MBA",
+    "count": 2
+  },
+  {
+    "Sector": "medical devices",
+    "Degree": "MD",
+    "count": 2
+  },
+  {
+    "Sector": "mobile",
+    "Degree": "BA",
+    "count": 2
+  },
+  {
+    "Sector": "mobile",
+    "Degree": "BS",
+    "count": 8
+  },
+  {
+    "Sector": "mobile",
+    "Degree": "MBA",
+    "count": 8
+  },
+  {
+    "Sector": "mobile",
+    "Degree": "MS",
+    "count": 5
+  },
+  {
+    "Sector": "mobile",
+    "Degree": "PhD",
+    "count": 4
+  },
+  {
+    "Sector": "public relations",
+    "Degree": "BA",
+    "count": 3
+  },
+  {
+    "Sector": "public relations",
+    "Degree": "BE",
+    "count": 3
+  },
+  {
+    "Sector": "public relations",
+    "Degree": "BS",
+    "count": 3
+  },
+  {
+    "Sector": "public relations",
+    "Degree": "MA",
+    "count": 3
+  },
+  {
+    "Sector": "public relations",
+    "Degree": "MBA",
+    "count": 8
+  },
+  {
+    "Sector": "public relations",
+    "Degree": "MS",
+    "count": 6
+  },
+  {
+    "Sector": "public relations",
+    "Degree": "PhD",
+    "count": 3
+  },
+  {
+    "Sector": "software",
+    "Degree": "BS",
+    "count": 1
+  },
+  {
+    "Sector": "software",
+    "Degree": "MBA",
+    "count": 5
+  },
+  {
+    "Sector": "software",
+    "Degree": "MS",
+    "count": 7
+  },
+  {
+    "Sector": "software",
+    "Degree": "PhD",
+    "count": 7
+  }
+];
+
+////////////////////////////////////////////////////////////
+///////////////////// Read in data /////////////////////////
+////////////////////////////////////////////////////////////
+			        
+
+	////////////////////////////////////////////////////////////
+	///////////////////// Prepare the data /////////////////////
+	////////////////////////////////////////////////////////////
+	
+	//Sort the inner characters based on the total number of words spoken
+	
+	//Find the total number of words per character
+	var dataChar = d3.nest()
+		.key(function(d) { return d.Sector; })
+		.rollup(function(leaves) { return d3.sum(leaves, function(d) { return d.count; }); })
+		.entries(dataAgg)
+		.sort(function(a, b){ return d3.descending(a.count, b.count); });				
+	//Unflatten the result
+	var characterOrder = dataChar.map(function(d) { return d.key; });
+	//Sort the characters on a specific order
+	function sortCharacter(a, b) {
+	  	return characterOrder.indexOf(a) - characterOrder.indexOf(b);
+	}//sortCharacter
+	
+	//Set more loom functions
+	loom
+		.sortSubgroups(sortCharacter)
+		.heightInner(innerRadius*0.75/characterOrder.length);
+	
+	////////////////////////////////////////////////////////////
+	///////////////////////// Colors ///////////////////////////
+	////////////////////////////////////////////////////////////
+					
+	//Color for the unique locations
+    	var locations = ["biotechnology", "enterprise software", "games", "health care",  "manufacturing", "medical", "medical devices", "mobile",  "public relations",   "software"];
+	var colors = ["#5a3511", "#47635f",   "#223e15", "#C6CAC9", "#0d1e25",  "#53821a",    "#4387AA",         "#770000", "#373F41", "#602317"];
+	var color = d3.scaleOrdinal()
+    	.domain(locations)
+    	.range(colors);
+	
+	//Create a group that already holds the data
+	var g = svg.append("g")
+	    .attr("transform", "translate(" + (width/2 + margin.left) + "," + (height/2 + margin.top) + ")")
+		.datum(loom(dataAgg));	
+
+	////////////////////////////////////////////////////////////
+	///////////////////// Set-up title /////////////////////////
+	////////////////////////////////////////////////////////////
+
+	var titles = g.append("g")
+		.attr("class", "texts")
+		.style("opacity", 0);
+		
+	titles.append("text")
+		.attr("class", "name-title")
+		.attr("x", 0)
+		.attr("y", -innerRadius*5/6);
+		
+	titles.append("text")
+		.attr("class", "value-title")
+		.attr("x", 0)
+		.attr("y", -innerRadius*5/6 + 25);
+	
+	//The character pieces	
+	titles.append("text")
+		.attr("class", "character-note")
+		.attr("x", 0)
+		.attr("y", innerRadius/2)
+		.attr("dy", "0.35em");
+					
+	////////////////////////////////////////////////////////////
+	////////////////////// Draw outer arcs /////////////////////
+	////////////////////////////////////////////////////////////
+
+	var arcs = g.append("g")
+	    .attr("class", "arcs")
+	  .selectAll("g")
+	    .data(function(s) { 
+			return s.groups; 
+		})
+	  .enter().append("g")
+		.attr("class", "arc-wrapper")
+	  	.each(function(d) { 
+			d.pullOutSize = (pullOutSize * ( d.startAngle > Math.PI + 1e-2 ? -1 : 1)) 
+		})
+ 	 	.on("mouseover", function(d) {
+			
+			//Hide all other arcs	
+			d3.selectAll(".arc-wrapper")
+		      	.transition()
+				.style("opacity", function(s) { return s.outername === d.outername ? 1 : 0.5; });
+			
+			//Hide all other strings
+		    d3.selectAll(".string")
+		      	.transition()
+		        .style("opacity", function(s) { return s.outer.outername === d.outername ? 1 : fadeOpacity; });
+				
+			//Find the data for the strings of the hovered over location
+			var locationData = loom(dataAgg).filter(function(s) { return s.outer.outername === d.outername; });
+			//Hide the characters who haven't said a word
+			d3.selectAll(".inner-label")
+		      	.transition()
+		        .style("opacity", function(s) {
+					//Find out how many words the character said at the hovered over location
+					var char = locationData.filter(function(c) { return c.outer.innername === s.Degree; });
+					return char.length === 0 ? 0.1 : 1;
+				});
+ 	 	})
+     	.on("mouseout", function(d) {
+			
+			//Sjow all arc labels
+			d3.selectAll(".arc-wrapper")
+		      	.transition()
+				.style("opacity", 1);
+			
+			//Show all strings again
+		    d3.selectAll(".string")
+		      	.transition()
+		        .style("opacity", defaultOpacity);
+				
+			//Show all characters again
+			d3.selectAll(".inner-label")
+		      	.transition()
+		        .style("opacity", 1);
+ 	 	});
+
+	var outerArcs = arcs.append("path")
+		.attr("class", "arc")
+	    .style("fill", function(d) { return color(d.outername); })
+	    .attr("d", arc)
+		.attr("transform", function(d, i) { //Pull the two slices apart
+		  	return "translate(" + d.pullOutSize + ',' + 0 + ")";
+		 });
+		 					
+	////////////////////////////////////////////////////////////
+	//////////////////// Draw outer labels /////////////////////
+	////////////////////////////////////////////////////////////
+
+	//The text needs to be rotated with the offset in the clockwise direction
+	var outerLabels = arcs.append("g")
+		.each(function(d) { d.angle = ((d.startAngle + d.endAngle) / 2); })
+		.attr("class", "outer-labels")
+		.attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+		.attr("transform", function(d,i) { 
+			var c = arc.centroid(d);
+			return "translate(" + (c[0] + d.pullOutSize) + "," + c[1] + ")"
+			+ "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+			+ "translate(" + 26 + ",0)"
+			+ (d.angle > Math.PI ? "rotate(180)" : "")
+		})
+		
+	//The outer name
+	outerLabels.append("text")
+		.attr("class", "outer-label")
+		.attr("dy", ".35em")
+		.text(function(d,i){ return d.outername; });
+		
+	//The value below it
+	outerLabels.append("text")
+		.attr("class", "outer-label-value")
+		.attr("dy", "1.5em")
+		.text(function(d,i){ return numFormat(d.value) + " startups"; });
+
+	////////////////////////////////////////////////////////////
+	////////////////// Draw inner strings //////////////////////
+	////////////////////////////////////////////////////////////
+	
+	var strings = g.append("g")
+	    .attr("class", "stringWrapper")
+		.style("isolation", "isolate")
+	  .selectAll("path")
+	    .data(function(strings) { 
+			return strings; 
+		})
+	  .enter().append("path")
+		.attr("class", "string")
+		.style("mix-blend-mode", "multiply")
+	    .attr("d", string)
+	    .style("fill", function(d) { return d3.rgb( color(d.outer.outername) ).brighter(0.2) ; })
+		.style("opacity", defaultOpacity);
+		
+	////////////////////////////////////////////////////////////
+	//////////////////// Draw inner labels /////////////////////
+	////////////////////////////////////////////////////////////
+			
+	//The text also needs to be displaced in the horizontal directions
+	//And also rotated with the offset in the clockwise direction
+	var innerLabels = g.append("g")
+		.attr("class","inner-labels")
+	  .selectAll("text")
+	    .data(function(s) { 
+			return s.innergroups; 
+		})
+	  .enter().append("text")
+		.attr("class", "inner-label")
+		.attr("x", function(d,i) { return d.x; })
+		.attr("y", function(d,i) { return d.y; })
+		.style("text-anchor", "middle")
+		.attr("dy", ".35em")
+	    .text(function(d,i) { return d.name; })
+ 	 	.on("mouseover", function(d) {
+			
+			//Show all the strings of the highlighted character and hide all else
+		    d3.selectAll(".string")
+		      	.transition()
+		        .style("opacity", function(s) {
+					return s.outer.innername !== d.name ? fadeOpacity : 1;
+				});
+				
+			//Update the word count of the outer labels
+			var characterData = loom(dataAgg).filter(function(s) { return s.outer.innername === d.Degree; });
+			d3.selectAll(".outer-label-value")
+				.text(function(s,i){
+					//Find which characterData is the correct one based on location
+					var loc = characterData.filter(function(c) { return c.outer.outername === s.outername; });
+					if(loc.length === 0) {
+						var value = 0;
+					} else {
+						var value = loc[0].outer.value;
+					}
+					return numFormat(value) + (value === 1 ? " startup" : " startups"); 
+					
+				});
+			
+			//Hide the arc where the character hasn't said a thing
+			d3.selectAll(".arc-wrapper")
+		      	.transition()
+		        .style("opacity", function(s) {
+					//Find which characterData is the correct one based on location
+					var loc = characterData.filter(function(c) { return c.outer.outername === s.outername; });
+					return loc.length === 0 ? 0.1 : 1;
+				});
+					
+			//Update the title to show the total word count of the character
+			d3.selectAll(".texts")
+				.transition()
+				.style("opacity", 1);	
+			d3.select(".name-title")
+				.text(d.name);
+			d3.select(".value-title")
+				.text(function() {
+					var words = dataChar.filter(function(s) { return s.key === d.name; });
+					return numFormat(words[0].value);
+				});
+				
+			//Show the character note
+			d3.selectAll(".character-note")
+				.text(characterNotes[d.name])
+				.call(wrap, 2.25*pullOutSize);
+				
+		})
+     	.on("mouseout", function(d) {
+			
+			//Put the string opacity back to normal
+		    d3.selectAll(".string")
+		      	.transition()
+				.style("opacity", defaultOpacity);
+				
+			//Return the word count to what it was
+			d3.selectAll(".outer-label-value")	
+				.text(function(s,i){ return numFormat(s.value) + " startups"; });
+				
+			//Show all arcs again
+			d3.selectAll(".arc-wrapper")
+		      	.transition()
+		        .style("opacity", 1);
+			
+			//Hide the title
+			d3.selectAll(".texts")
+				.transition()
+				.style("opacity", 0);
+			
+		});
+	  	
+
+////////////////////////////////////////////////////////////
+///////////////////// Extra functions //////////////////////
+////////////////////////////////////////////////////////////
+
+//Sort alphabetically
+function sortAlpha(a, b){
+	    if(a < b) return -1;
+	    if(a > b) return 1;
+	    return 0;
+}//sortAlpha
+
+//Sort on the number of words
+function sortWords(a, b){
+	    if(a.count < b.count) return -1;
+	    if(a.count > b.count) return 1;
+	    return 0;
+}//sortWords
+
+/*Taken from http://bl.ocks.org/mbostock/7555321
+//Wraps SVG text*/
+function wrap(text, width) {
+  text.each(function() {
+	var text = d3.select(this),
+		words = text.text().split(/\s+/).reverse(),
+		word,
+		line = [],
+		lineNumber = 0,
+		lineHeight = 1.2, // ems
+		y = parseFloat(text.attr("y")),
+		x = parseFloat(text.attr("x")),
+		dy = parseFloat(text.attr("dy")),
+		tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+
+	while (word = words.pop()) {
+	  line.push(word);
+	  tspan.text(line.join(" "));
+	  if (tspan.node().getComputedTextLength() > width) {
+		line.pop();
+		tspan.text(line.join(" "));
+		line = [word];
+		tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+	  }
+	}
+  });
+}//wrap
